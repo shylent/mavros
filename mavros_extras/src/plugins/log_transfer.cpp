61d406baa80669297847e0f9df1ea71fd0edc52e
mlvov <mlvov@cnord.ru>
2018-09-04 14:12:39 +0300

    WIP

--- mavros_extras/src/plugins/log_transfer.cpp
+++ mavros_extras/src/plugins/log_transfer.cpp
@@ -1,16 +1,32 @@
+#include <stack>
+
 #include <mavros/mavros_plugin.h>
 #include <mavros_msgs/LogData.h>
 #include <mavros_msgs/LogEntry.h>
 #include <mavros_msgs/LogRequestData.h>
 #include <mavros_msgs/LogRequestEnd.h>
 #include <mavros_msgs/LogRequestList.h>
+#include <mavros_msgs/GetLogListAction.h>
+#include <mavros_msgs/LogListItem.h>
+#include <mavros_msgs/GetLogAction.h>
+
+#include <actionlib/server/simple_action_server.h>
 
 namespace mavros {
 namespace extra_plugins {
 class LogTransferPlugin : public plugin::PluginBase {
+private:
+	using GetLogListActionServer = actionlib::SimpleActionServer<mavros_msgs::GetLogListAction>;
+	GetLogListActionServer get_log_list_action_srv;
+
+	using GetLogActionServer = actionlib::SimpleActionServer<mavros_msgs::GetLogAction>;
+	GetLogActionServer get_log_action_srv;
+
 public:
 	LogTransferPlugin() :
-		nh("~log_transfer") {}
+		nh("~log_transfer"),
+		get_log_list_action_srv(nh, "get_log_list", false),
+		get_log_action_srv(nh, "get_log", false) {}
 
 	void initialize(UAS& uas) override;
 
@@ -21,6 +37,15 @@ private:
 	ros::Publisher log_entry_pub, log_data_pub;
 	ros::ServiceServer log_request_list_srv, log_request_data_srv, log_request_end_srv;
 
+	enum class State {
+		Idle,
+		GettingLogList,
+		GettingLogData,
+	};
+	State state = State::Idle;
+
+	std::stack<mavros_msgs::LogListItem> logListItems;
+
 	void handle_log_entry(const mavlink::mavlink_message_t *msg, mavlink::common::msg::LOG_ENTRY& le);
 	void handle_log_data(const mavlink::mavlink_message_t *msg, mavlink::common::msg::LOG_DATA& ld);
 	bool log_request_list_cb(mavros_msgs::LogRequestList::Request &req,
@@ -29,6 +54,15 @@ private:
 				mavros_msgs::LogRequestData::Response &res);
 	bool log_request_end_cb(mavros_msgs::LogRequestEnd::Request &req,
 				mavros_msgs::LogRequestEnd::Response &res);
+
+	bool stopLogTransfer();
+
+	void logListRequested();
+	void logListRequestPreempted();
+	void logRequested();
+	void logRequestPreempted();
+
+	static constexpr char* logName = "log_transfer";
 };
 
 void LogTransferPlugin::initialize(UAS& uas)
@@ -44,13 +78,21 @@ void LogTransferPlugin::initialize(UAS& uas)
 				&LogTransferPlugin::log_request_data_cb, this);
 	log_request_end_srv = nh.advertiseService("raw/log_request_end",
 				&LogTransferPlugin::log_request_end_cb, this);
+
+	get_log_list_action_srv.registerGoalCallback(boost::bind(&LogTransferPlugin::logListRequested, this));
+	get_log_list_action_srv.registerPreemptCallback(boost::bind(&LogTransferPlugin::logListRequestPreempted, this));
+	get_log_list_action_srv.start();
+
+	get_log_action_srv.registerGoalCallback(boost::bind(&LogTransferPlugin::logRequested, this));
+	get_log_action_srv.registerPreemptCallback(boost::bind(&LogTransferPlugin::logRequestPreempted, this));
+	get_log_action_srv.start();
 }
 
 plugin::PluginBase::Subscriptions LogTransferPlugin::get_subscriptions()
 {
 	return {
-		make_handler(&LogTransferPlugin::handle_log_entry),
-		make_handler(&LogTransferPlugin::handle_log_data),
+		       make_handler(&LogTransferPlugin::handle_log_entry),
+		       make_handler(&LogTransferPlugin::handle_log_data),
 	};
 }
 
@@ -121,16 +163,70 @@ bool LogTransferPlugin::log_request_data_cb(mavros_msgs::LogRequestData::Request
 bool LogTransferPlugin::log_request_end_cb(mavros_msgs::LogRequestEnd::Request&,
 			mavros_msgs::LogRequestEnd::Response& res)
 {
+	res.success = stopLogTransfer();
+	return true;
+}
+
+bool LogTransferPlugin::stopLogTransfer()
+{
 	mavlink::common::msg::LOG_REQUEST_END msg;
 	m_uas->msg_set_target(msg);
-	res.success = true;
 	try {
 		UAS_FCU(m_uas)->send_message(msg);
 	} catch (std::length_error&) {
-		res.success = false;
+		ROS_ERROR_NAMED(logName, "Failed to send LOG_REQUEST_END message");
+		return false;
 	}
 	return true;
 }
+
+void LogTransferPlugin::logListRequested()
+{
+	auto goal = get_log_list_action_srv.acceptNewGoal();
+	if (goal == nullptr) {
+		ROS_WARN_NAMED(logName, "Null goal received for get_log_list");
+		return;
+	}
+
+	stopLogTransfer();
+	if (get_log_action_srv.isActive()) {
+		mavros_msgs::GetLogResult res;
+		res.success = false;
+		get_log_action_srv.setPreempted(res, "Log list was requested");
+	}
+	state = State::Idle;
+	if (get_log_list_action_srv.isPreemptRequested()) {
+		mavros_msgs::GetLogListResult res;
+		res.success = false;
+		get_log_list_action_srv.setPreempted(res, "Log list request was preempted");
+		return;
+	}
+
+}
+
+void LogTransferPlugin::logListRequestPreempted()
+{
+
+}
+
+void LogTransferPlugin::logRequested()
+{
+	auto goal = get_log_action_srv.acceptNewGoal();
+	if (get_log_list_action_srv.isPreemptRequested()) {
+		mavros_msgs::GetLogListResult res;
+		res.success = false;
+		get_log_list_action_srv.setPreempted(res, "Another log list request was accepted");
+		if (get_log_action_srv.isActive()) {
+			mavros_msgs::GetLogResult res;
+			res.success = false;
+			get_log_action_srv.setPreempted(res, "Log list was requested");
+		}
+		return;
+	}
+}
+
+void LogTransferPlugin::logRequestPreempted()
+{}
 }	// namespace extra_plugins
 }	// namespace mavros
 
